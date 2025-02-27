b3259c49a5cdbf91c4fec059db5dcf79bc9e9e55
navdeep111 <guptanavdeep9461@gmail.com>
2025-02-27 22:19:59 +0530

    server.js changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,44 @@
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import connectDB from './config/db.js';  // Note the `.js` extension for ES6 module compatibility
+// import authRoutes from './routes/authRoutes.js';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+// import multer from 'multer';
+// import path from 'path';
+// import crypto from 'crypto';
+
+// const app = express();
+
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// const buildPath = path.join(__dirname, '../frontend/dist');
+// app.use(express.static(buildPath));
+
+// app.use(bodyParser.json());
+// app.use(cors({origin: ['']}));
+
+
+// connectDB();
+
+// app.use(express.json({ extended: false }));
+
+// // Define Routes
+// app.use('/api/auth', authRoutes);
+// app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html')); });
+
+
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
+// -----------------------------------
+
+
+
 import express from 'express';
 import mongoose from 'mongoose';
 import connectDB from './config/db.js';  // Note the `.js` extension for ES6 module compatibility
@@ -10,12 +51,12 @@ import crypto from 'crypto';
 
 const app = express();
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const buildPath = path.join(__dirname, '../frontend/dist');
-app.use(express.static(buildPath));
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// const buildPath = path.join(__dirname, '../frontend/dist');
+// app.use(express.static(buildPath));
 
 app.use(bodyParser.json());
 app.use(cors({origin: ['']}));
@@ -27,8 +68,10 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/auth', authRoutes);
-app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html')); });
-
+// app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html')); });
+app.get('/',(req,res)=>{
+    res.send('Hello Backend is running')
+})
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
